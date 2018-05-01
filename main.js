8acd3686775266229ec2a5b76e1b310e63e77a44
Ivan Flores <ivan@Ivans-MacBook-Pro.local>
2018-05-01 19:27:43 -0400

    all

--- main.js
+++ main.js
@@ -8,12 +8,28 @@ $(document).ready(function(){
         }
       });
       // var filter = $(this).val();
-//var totalCostMoneyFormat = format(totalCost,"$");
-//console.log(totalCostMoneyFormat);
-$(".TotalFines").find("span").text(format(totalCost, "$"));
-    //  $(".item.active").removeClass("active");
+    //var totalCostMoneyFormat = format(totalCost,"$");
+    //console.log(totalCostMoneyFormat);
+    $(".TotalFines").find("span").text(format(totalCost, "$"));
+      //  $(".item.active").removeClass("active");
       //$("." + filter).addClass("active");
   });
+
+  $("[type='checkbox']").on("click", function(e){
+    console.log("FIRE! (movie theater is empty)");
+    $(".law").removeClass("active");
+
+    $("[type='checkbox']:checked").each(function(){
+      console.log("Checked");
+        var this_box = $(this),
+          id = this_box.data("id");
+
+
+        $("#" + id).addClass("active");
+    });
+  });
+
+
   function format(n, currency) {
     return currency + n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
   }
